af62cf43120c65cc31d815f14fa5121c9a626376
Daniel E. Doherty <ded-law@ddoherty.net>
2022-11-22 11:40:04 -0600

    Add minimal useful spec.

--- spec/git_auto_spec.rb
+++ spec/git_auto_spec.rb
@@ -5,7 +5,17 @@ RSpec.describe GitAuto do
     expect(GitAuto::VERSION).not_to be nil
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  let(:ga) { GitAuto.new("~/src/fat_core", "~/src/fat_period") }
+
+  it 'initializes with a list of directories' do
+    expect(ga.dirs).to include("/home/ded/src/fat_core")
+    expect(ga.dirs).to include("/home/ded/src/fat_period")
+  end
+
+  it "reads environment variables from ENV GIT_AUTO_ENV_PATH" do
+    expect(ga.getenv['HOME']).to match(/ded/)
+  end
+
+  it "merges in environment variables from files in GIT_AUTO_ENV_PATH" do
   end
 end
